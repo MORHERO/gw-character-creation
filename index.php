978ccd1ea858cd14f24c53804ecdc6f149cfd806
MORHERO <m.rimkus1997@gmail.com>
2020-08-16 18:27:50 +0200

    imported base file structure from gw-gamemaster-interface

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CHARACTER CREATION</title>
+	<meta charset="utf-8">
+	<?php
+		include 'templates/styles.php';
+	?>
+</head>
+<body>
+	<?php
+		include 'templates/header.php';
+		include 'templates/navigation.php';
+	?>
+
+	<main>
+		<div class="inner">
+			<div class="main-flex">
+			</div>
+		</div>
+	</main>
+	
+	<?php
+		include 'templates/footer.php';
+		include 'templates/scripts.php';
+	?>
+</body>
+</html>
